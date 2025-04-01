7374ab8297a7b3ffe605507848d659c71f090e7d
Arturo <arturotinajas@gmail.com>
2025-04-02 01:53:48 +0200

    Changes

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="title-header"></div>
+      <div className="nav-products"></div>
+      <div className="products"></div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ProductImage/productImage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './product-image.css';
+
+const ProductImage = () => {
+  return (
+    <div className="product-image">
+      <h1>ProductImage Component</h1>
+    </div>
+  );
+};
+
+export default ProductImage;
