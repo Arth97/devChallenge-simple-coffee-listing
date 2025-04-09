30578cfe8d4a07a446c7ab42d6db353e7be8d6e2
Arturo <arturotinajas@gmail.com>
2025-04-09 21:52:13 +0200

    Fix

--- src/components/CollectionHeader/collectionHeader.jsx
+++ src/components/CollectionHeader/collectionHeader.jsx
@@ -3,7 +3,7 @@ import './collectionHeader.css';
 
 const CollectionHeader = () => {
   return (
-    <div className="flex flex-col gap-2 mb-5">
+    <div className="flex flex-col items-center gap-2 mb-5">
       <h1 className="heading">Our Collection</h1>
       <p>
         Introducing our Coffee Collection, a selection of unique coffees from different roast types and origins, expertly roasted in small batches and shipped fresh weekly.
